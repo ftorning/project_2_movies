4c4dc65e65ea45bb0d077a0efcaf9ab80b71e1cb
Fraser Torning <ftorning@gmail.com>
2015-08-26 22:28:01 -0400

    final code for submission

--- tournament.sql
+++ tournament.sql
@@ -1,9 +1,32 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS matches;
+
+CREATE TABLE Players ( id SERIAL primary key, name TEXT );
+
+CREATE TABLE Matches ( id SERIAL primary key, player int references Players(id),
+						opponent int references Players(id), result int);
+
+-- view shows the number of wins for each player
+CREATE VIEW wins AS
+	SELECT Players.id, COUNT(matches.opponent) AS n 
+	FROM Players
+	LEFT JOIN (SELECT * FROM Matches WHERE result>0) as Matches
+	ON Players.id = Matches.player
+	GROUP BY Players.id;
+
+-- view of number of matches for each player
+CREATE VIEW count AS
+	SELECT Players.id, COUNT(Matches.opponent) AS n 
+	FROM Players
+	LEFT JOIN Matches
+	ON Players.id = Matches.player
+	GROUP BY Players.id;
+
+-- view both wins and count for each player
+CREATE VIEW standings AS 
+	SELECT Players.id, Players.name, wins.n as wins, count.n as matches 
+	FROM Players, count, wins
+	WHERE Players.id = wins.id and wins.id = count.id;
+
 
 
